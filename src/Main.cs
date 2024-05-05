bf40b4063d8c13413c278e726d804f964357e92b
Matt Mannion <mattpmannion@gmail.com>
2024-05-05 02:15:39 -0400

    Update Main to leverage new root resolver methods

--- src/Main.cs
+++ src/Main.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddGraphQLServer()
-    .AddQueryType<BookQuery>();
+    .AddQueries()
+    .AddMutations();
 
 var app = builder.Build();
 
