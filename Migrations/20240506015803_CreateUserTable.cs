973d1af6c39f4182ba02fcefcb1581565bb74ed4
Matt Mannion <mattpmannion@gmail.com>
2024-05-05 22:48:49 -0400

    Add CreateUserTable Migrations

---
+++ Migrations/20240506015803_CreateUserTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace gql.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstname = table.Column<string>(type: "varchar(50)", nullable: false),
+                    lastname = table.Column<string>(type: "varchar(50)", nullable: false),
+                    username = table.Column<string>(type: "varchar(50)", nullable: false),
+                    email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    password = table.Column<string>(type: "varchar(255)", nullable: true),
+                    confirmed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
